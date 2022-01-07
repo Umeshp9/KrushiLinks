2cab44c0f159319dbe5828876dc55c84224439e7
Umeshp9 <umhaske62@gmail.com>
2022-01-07 09:34:05 +0530

    first commit

---
+++ src/app/shared/sidebar/menu-items.ts
@@ -0,0 +1,85 @@
+import { RouteInfo } from './sidebar.metadata';
+
+export const ROUTES: RouteInfo[] = [
+ 
+  {
+    path: '/dashboard',
+    title: 'मुख्य मेनू',
+    icon: 'bi bi-speedometer2',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/alert',
+    title: 'Alert',
+    icon: 'bi bi-bell',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/badges',
+    title: 'खते',
+    icon: 'bi bi-patch-check',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/buttons',
+    title: 'बिबियाने',
+    icon: 'bi bi-hdd-stack',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/card',
+    title: 'संपर्क',
+    icon: 'bi bi-card-text',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/dropdown',
+    title: 'आमच्याबद्दल माहिती',
+    icon: 'bi bi-menu-app',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/pagination',
+    title: 'Pagination',
+    icon: 'bi bi-dice-1',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/nav',
+    title: 'Nav',
+    icon: 'bi bi-pause-btn',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/table',
+    title: 'Table',
+    icon: 'bi bi-layout-split',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/about',
+    title: 'About',
+    icon: 'bi bi-people',
+    class: '',
+    extralink: false,
+    submenu: []
+  }
+];
